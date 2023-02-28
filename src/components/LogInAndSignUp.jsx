f172736b0b107fa91651af329d47f3e7ae6feb3e
Brag <brangtsawmaung89@gmail.com>
2023-02-28 22:31:42 +0630

    ee

--- src/components/LogInAndSignUp.jsx
+++ src/components/LogInAndSignUp.jsx
@@ -6,11 +6,11 @@ import SignUp from './SignUp';
 function LogInAndSignUp() {
   return (
     <div className='w-full h-screen flex justify-center items-center'>
-      <LoginForm />
-        {/* <Routes>
+      {/* <LoginForm /> */}
+        <Routes>
             <Route path='/' element={<LoginForm />} />
             <Route path='/SignUp' element={<SignUp />}/>
-        </Routes> */}
+        </Routes>
     </div>
   )
 }
